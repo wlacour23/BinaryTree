3726e7d5927684b533e133a7952d6b266a322952
wlacour23 <74622073+wlacour23@users.noreply.github.com>
2021-01-21 14:01:36 -0600

    Add files via upload
    The lab done in IntelliJ

---
+++ src/com/tts/Main.java
@@ -0,0 +1,12 @@
+package com.tts;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
